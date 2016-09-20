821314345d7c67f6686a52038da1823970b014a7
Cuenta Generica para introdución a la neurociencia <introneuro-12@ws18.labo1>
2016-09-20 17:02:09 -0300

    Agrego funcion que genera los bloques

---
+++ bloques.m
@@ -0,0 +1,15 @@
+%bloque 1
+%el sujeto  solamente ve imagen
+img = 1
+snd = 0
+delay = 0
+target = 1
+Imagenes(delay, img, snd,target)
+%bloque 2
+%el sujeto  solamente ve sondio
+img = 0
+snd = 1
+delay = 0
+target = 0
+Imagenes(delay, img, snd,target)
+
