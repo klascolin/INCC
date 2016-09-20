d67a85ff4b53e027bc4b935eddc66e33771f61ae
Cuenta Generica para introdución a la neurociencia <introneuro-12@ws22.labo7>
2016-09-20 17:47:40 -0300

    Agrego funcion que arma los 9 bloques

--- bloques.m
+++ bloques.m
@@ -12,4 +12,53 @@ snd = 1
 delay = 0
 target = 0
 Imagenes(delay, img, snd,target)
+%bloque 3
+%el sujeto ve imagen y sonido sincronizado
+img = 1
+snd = 1
+delay = 0
+target = 1 % no importa
+Imagenes(delay, img, snd,target)
+%bloque 4
+%el sujeto ve imagen y sonido + 0.25, sigue a la imagen
+img = 1
+snd = 1
+delay = 0.25
+target = 1 
+Imagenes(delay, img, snd,target)
+%bloque 5
+%el sujeto ve imagen y sonido + 0.5, sigue a la imagen
+img = 1
+snd = 1
+delay = 0.5
+target = 1 
+Imagenes(delay, img, snd,target)
+%bloque 6
+%el sujeto ve imagen y sonido + 0.75, sigue a la imagen
+img = 1
+snd = 1
+delay = 0.75
+target = 1 
+Imagenes(delay, img, snd,target)
+%bloque 7
+%el sujeto ve imagen y sonido -0.25 , sigue al sonido
+img = 1
+snd = 1
+delay = 0.25
+target = 0 
+Imagenes(delay, img, snd,target)
+%bloque 8
+%el sujeto ve imagen y sonido -0.5 , sigue al sonido
+img = 1
+snd = 1
+delay = 0.5
+target = 0 
+Imagenes(delay, img, snd,target)
+%bloque 9
+%el sujeto ve imagen y sonido -0.75 , sigue al sonido
+img = 1
+snd = 1
+delay = 0.75
+target = 0 
+Imagenes(delay, img, snd,target)
 
