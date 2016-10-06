7c23f6d9d629992de65424bd789813987b42cf64
Cuenta Generica para introdución a la neurociencia <introneuro-12@ws15.labo4>
2016-10-06 19:10:52 -0300

    analisis

--- analisis.m
+++ analisis.m
@@ -1,5 +1,6 @@
 format long g;
 
+%% Leer CSV
 i = 0;
 sujeto = [];
 tiempo = [];
@@ -12,8 +13,8 @@ total_trials = [];
 target_time = cell(1,1);
 time_firstPress = cell(1,1);
 time_lastPress = cell(1,1);
-time_delta = cell(1,1);
 f = fopen('data/bloques.csv');
+
 while 1    
     suj = fscanf(f, '%i,', [1 1]);
     if isempty(suj)
@@ -39,19 +40,76 @@ while 1
 end
 fclose(f);
 
-num_bloques = i;
 
-n_invalidos = 0;
-for i = 1 : num_bloques
+%% Meter cada trial en el struct Trials
+num_bloques = i;
+j = 1;
+s = 0;
+Trials = struct();
+for b = 1 : num_bloques
+   if s ~= sujeto(b)
+       bloque_del_sujeto = 1;
+       s = sujeto(b);
+   else
+       bloque_del_sujeto = bloque_del_sujeto + 1;
+   end
+   deltas = time_firstPress{b} - target_time{b};
+   deltas(find(deltas < -100000)) = [];
+   delta_bloque(b) = mean(deltas);
    
-   invalidos = find(time_firstPress{i} == -1);
-   n_invalidos = n_invalidos + length(invalidos);
-  
-   target_time{i}(invalidos) = [];
-   time_firstPress{i}(invalidos) = [];
-   time_lastPress{i}(invalidos) = [];
-   time_delta{i} = time_firstPress{i} - target_time{i};
-   
-   disp(mean(time_delta{i}))
+   for k = 1 : total_trials(b)
+
+       Trials(j).Sujeto = sujeto(b);
+       Trials(j).Tiempo = tiempo(b);
+       Trials(j).EsDePractica = practica(b);
+       Trials(j).Delay = delay(b);
+       Trials(j).HayImagen = img(b);
+       Trials(j).HaySonido = snd(b);
+       Trials(j).SeguirImagen = target(b);
+       Trials(j).NumBloque = bloque_del_sujeto;
+       Trials(j).NumBloqueOriginal = b;
+       Trials(j).NumTrial = k;
+       
+       Trials(j).TiempoObjetivo = target_time{b}(k);
+       Trials(j).PrimerTap = time_firstPress{b}(k);
+       Trials(j).UltimoTap = time_lastPress{b}(k);
+
+       j = j + 1;    
+   end
 end
-disp(n_invalidos)
+
+clear sujeto tiempo practica delay img snd target bloque_del_sujeto total_trials target_time time_firstPress time_lastPress f b i j k s num_bloques suj
+
+%% Plot de todos los deltas
+for s = 1001 : 1010
+    j = 1;
+    deltas = [];
+    ultimo_nbo = 0;
+	for i = 1 : size(Trials, 2)
+	    if Trials(i).Sujeto ~= s % mirar solo un sujeto
+	        continue
+	    end
+	    
+	    if Trials(i).PrimerTap == -1 % no apreto nada
+	        continue
+	    end
+	   
+	    delta = Trials(i).PrimerTap - Trials(i).TiempoObjetivo;
+	    
+	    if delta < -0.5  % sacar picos en -2
+	        continue
+	    end
+	    nbo = Trials(i).NumBloqueOriginal;
+        if nbo == ultimo_nbo
+            continue
+        end
+        ultimo_nbo = nbo;
+	    deltas(j) = delta_bloque(nbo);
+        j = j + 1;
+	end
+    if ~isempty(deltas)
+        figure;
+        plot(deltas);
+    end
+	clear i j deltas
+end
